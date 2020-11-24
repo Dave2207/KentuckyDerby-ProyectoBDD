1bb6ad4d2386a55bb368510c0a701b3418eafce0
Dave2207 <dave400163@gmail.com>
2020-11-24 18:59:23 -0400

    Completando clases Race y Ticket

--- ProyectoBDD/src/logic/Race.java
+++ ProyectoBDD/src/logic/Race.java
@@ -1,5 +1,68 @@
 package logic;
 
 public class Race {
+	private int raceID;
+	private int distance;
+	private String raceType;
+	private String raceDate;
+	private String raceTime;
+	private String weather;
+	
+	public Race(int raceID, int distance, String raceType, String raceDate, String raceTime, String weather) {
+		super();
+		this.raceID = raceID;
+		this.distance = distance;
+		this.raceType = raceType;
+		this.raceDate = raceDate;
+		this.raceTime = raceTime;
+		this.weather = weather;
+	}
 
+	public int getRaceID() {
+		return raceID;
+	}
+
+	public void setRaceID(int raceID) {
+		this.raceID = raceID;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+
+	public String getRaceType() {
+		return raceType;
+	}
+
+	public void setRaceType(String raceType) {
+		this.raceType = raceType;
+	}
+
+	public String getRaceDate() {
+		return raceDate;
+	}
+
+	public void setRaceDate(String raceDate) {
+		this.raceDate = raceDate;
+	}
+
+	public String getRaceTime() {
+		return raceTime;
+	}
+
+	public void setRaceTime(String raceTime) {
+		this.raceTime = raceTime;
+	}
+
+	public String getWeather() {
+		return weather;
+	}
+
+	public void setWeather(String weather) {
+		this.weather = weather;
+	}
 }
--- ProyectoBDD/src/logic/Ticket.java
+++ ProyectoBDD/src/logic/Ticket.java
@@ -1,5 +1,91 @@
 package logic;
 
 public class Ticket {
+	private int code;
+	private String paymentMode;
+	private float price;
+	private Horse BetHorse;
+	private String DateRace;
+	private String TimeRace;
+	private String DatePurchase;
+	private String TimePurchase;
+	
+	public Ticket(int code, String paymentMode, float price, Horse betHorse, String dateRace, String timeRace,
+			String datePurchase, String timePurchase) {
+		super();
+		this.code = code;
+		this.paymentMode = paymentMode;
+		this.price = price;
+		BetHorse = betHorse;
+		DateRace = dateRace;
+		TimeRace = timeRace;
+		DatePurchase = datePurchase;
+		TimePurchase = timePurchase;
+	}
 
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getPaymentMode() {
+		return paymentMode;
+	}
+
+	public void setPaymentMode(String paymentMode) {
+		this.paymentMode = paymentMode;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public Horse getBetHorse() {
+		return BetHorse;
+	}
+
+	public void setBetHorse(Horse betHorse) {
+		BetHorse = betHorse;
+	}
+
+	public String getDateRace() {
+		return DateRace;
+	}
+
+	public void setDateRace(String dateRace) {
+		DateRace = dateRace;
+	}
+
+	public String getTimeRace() {
+		return TimeRace;
+	}
+
+	public void setTimeRace(String timeRace) {
+		TimeRace = timeRace;
+	}
+
+	public String getDatePurchase() {
+		return DatePurchase;
+	}
+
+	public void setDatePurchase(String datePurchase) {
+		DatePurchase = datePurchase;
+	}
+
+	public String getTimePurchase() {
+		return TimePurchase;
+	}
+
+	public void setTimePurchase(String timePurchase) {
+		TimePurchase = timePurchase;
+	}
+	
+	
 }
