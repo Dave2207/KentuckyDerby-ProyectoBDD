f02ba64e34884f04bf4d91640fc7d948628f10a8
Usuario <Usuario@DESKTOP-EJHCJ3D>
2020-11-24 23:54:32 +0100

    nada relevante

--- ProyectoBDD/src/visual/Login.java
+++ ProyectoBDD/src/visual/Login.java
@@ -7,10 +7,14 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Login extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -29,11 +33,30 @@ public class Login extends JDialog {
 	 * Create the dialog.
 	 */
 	public Login() {
+		setTitle("Login");
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblUsuario = new JLabel("Usuario: ");
+		lblUsuario.setBounds(22, 39, 56, 16);
+		contentPanel.add(lblUsuario);
+		
+		textField = new JTextField();
+		textField.setBounds(114, 36, 116, 22);
+		contentPanel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a: ");
+		lblContrasea.setBounds(22, 104, 84, 16);
+		contentPanel.add(lblContrasea);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(114, 101, 116, 22);
+		contentPanel.add(textField_1);
+		textField_1.setColumns(10);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -51,5 +74,4 @@ public class Login extends JDialog {
 			}
 		}
 	}
-
 }
