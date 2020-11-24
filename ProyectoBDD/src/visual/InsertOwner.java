6ad1727546393b89e288b51849a1c0c9048d344c
Isara Rosmel <Isara Rosmel@Toshiba>
2020-11-24 16:53:04 -0400

    poner elementos en ventana visual de insertar trainer y owner

--- ProyectoBDD/src/visual/InsertOwner.java
+++ ProyectoBDD/src/visual/InsertOwner.java
@@ -7,10 +7,19 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
+import java.awt.Color;
 
 public class InsertOwner extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField txtIdOwner;
+	private JTextField txtNombreOwner;
+	private JTextField txtApellidoOwner;
+	private JTextField txtStateResOwner;
 
 	/**
 	 * Launch the application.
@@ -29,11 +38,64 @@ public class InsertOwner extends JDialog {
 	 * Create the dialog.
 	 */
 	public InsertOwner() {
-		setBounds(100, 100, 450, 300);
+		setTitle("Registro Owner");
+		setBounds(100, 100, 486, 431);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel label = new JLabel("ID: ");
+			label.setBounds(97, 43, 18, 14);
+			contentPanel.add(label);
+		}
+		{
+			txtIdOwner = new JTextField();
+			txtIdOwner.setEnabled(false);
+			txtIdOwner.setEditable(false);
+			txtIdOwner.setColumns(10);
+			txtIdOwner.setBounds(125, 37, 86, 20);
+			contentPanel.add(txtIdOwner);
+		}
+		{
+			JLabel label = new JLabel("Nombre:");
+			label.setBounds(69, 71, 46, 14);
+			contentPanel.add(label);
+		}
+		{
+			txtNombreOwner = new JTextField();
+			txtNombreOwner.setColumns(10);
+			txtNombreOwner.setBounds(125, 68, 186, 20);
+			contentPanel.add(txtNombreOwner);
+		}
+		{
+			JLabel label = new JLabel("Apellidos:");
+			label.setBounds(69, 96, 46, 14);
+			contentPanel.add(label);
+		}
+		{
+			txtApellidoOwner = new JTextField();
+			txtApellidoOwner.setColumns(10);
+			txtApellidoOwner.setBounds(125, 93, 186, 20);
+			contentPanel.add(txtApellidoOwner);
+		}
+		{
+			JLabel label = new JLabel("State Residence:");
+			label.setBounds(29, 127, 86, 14);
+			contentPanel.add(label);
+		}
+		{
+			txtStateResOwner = new JTextField();
+			txtStateResOwner.setColumns(10);
+			txtStateResOwner.setBounds(125, 124, 263, 20);
+			contentPanel.add(txtStateResOwner);
+		}
+		{
+			JPanel panel = new JPanel();
+			panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Owners", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+			panel.setBounds(10, 160, 449, 176);
+			contentPanel.add(panel);
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
--- ProyectoBDD/src/visual/InsertTrainer.java
+++ ProyectoBDD/src/visual/InsertTrainer.java
@@ -7,10 +7,17 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
 
 public class InsertTrainer extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField txtIdTrainer;
+	private JTextField txtNombreTrainer;
+	private JTextField txtApellidoTrainer;
+	private JTextField txtStateResTrainer;
 
 	/**
 	 * Launch the application.
@@ -29,11 +36,55 @@ public class InsertTrainer extends JDialog {
 	 * Create the dialog.
 	 */
 	public InsertTrainer() {
-		setBounds(100, 100, 450, 300);
+		setTitle("Registro Trainer");
+		setBounds(100, 100, 484, 411);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblId = new JLabel("ID: ");
+		lblId.setBounds(95, 36, 18, 14);
+		contentPanel.add(lblId);
+		
+		txtIdTrainer = new JTextField();
+		txtIdTrainer.setEnabled(false);
+		txtIdTrainer.setEditable(false);
+		txtIdTrainer.setBounds(123, 30, 86, 20);
+		contentPanel.add(txtIdTrainer);
+		txtIdTrainer.setColumns(10);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(67, 64, 46, 14);
+		contentPanel.add(lblNombre);
+		
+		txtNombreTrainer = new JTextField();
+		txtNombreTrainer.setBounds(123, 61, 186, 20);
+		contentPanel.add(txtNombreTrainer);
+		txtNombreTrainer.setColumns(10);
+		
+		JLabel lblApellidos = new JLabel("Apellidos:");
+		lblApellidos.setBounds(67, 89, 46, 14);
+		contentPanel.add(lblApellidos);
+		
+		txtApellidoTrainer = new JTextField();
+		txtApellidoTrainer.setBounds(123, 86, 186, 20);
+		contentPanel.add(txtApellidoTrainer);
+		txtApellidoTrainer.setColumns(10);
+		
+		JLabel lblStateResidence = new JLabel("State Residence:");
+		lblStateResidence.setBounds(27, 120, 86, 14);
+		contentPanel.add(lblStateResidence);
+		
+		txtStateResTrainer = new JTextField();
+		txtStateResTrainer.setBounds(123, 117, 263, 20);
+		contentPanel.add(txtStateResTrainer);
+		txtStateResTrainer.setColumns(10);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Trainers", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(10, 152, 449, 176);
+		contentPanel.add(panel);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -51,5 +102,4 @@ public class InsertTrainer extends JDialog {
 			}
 		}
 	}
-
 }
