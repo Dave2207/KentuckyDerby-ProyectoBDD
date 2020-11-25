0585e68adbe4857a31e865c39ae65074002c5a0d
Dave2207 <dave400163@gmail.com>
2020-11-24 22:42:17 -0400

    Creo clase Derby (controladora)

---
+++ ProyectoBDD/src/logic/Derby.java
@@ -0,0 +1,13 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class Derby {
+	private ArrayList<Employee> employees;
+	private ArrayList<Horse> horses;
+	private ArrayList<Jockey> jockeys;
+	private ArrayList<Owner> owners;
+	private ArrayList<Trainer> trainers;
+	
+	
+}
