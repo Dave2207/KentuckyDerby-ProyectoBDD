16dec18e638be0dbde59db34acbf159c854c2e7a
Dave2207 <dave400163@gmail.com>
2020-11-26 16:47:42 -0400

    Conexion a base de datos SE LOGRA

---
+++ ProyectoBDD/src/SQL/hola.java
@@ -0,0 +1,43 @@
+package SQL;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class hola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        Connection conn = null;
+ 
+        try {
+ 
+            String dbURL = "jdbc:sqlserver://localhost";
+            String user = "DavidV";
+            String pass = "2271999";
+            conn = DriverManager.getConnection(dbURL, user, pass);
+            if (conn != null) {
+                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+            }
+ 
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+	
+	}
+
+}
